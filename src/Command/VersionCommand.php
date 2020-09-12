a75edcecff97d67647c342fc22b2b80333e7b7e3
marvin255 <postbox433@gmail.com>
2020-09-12 23:43:15 +0200

    add VersionCommand

---
+++ src/Command/VersionCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Liquetsoft\Fias\Laravel\LiquetsoftFiasBundle\Command;
+
+use Illuminate\Console\Command;
+use Illuminate\Foundation\Application;
+use Liquetsoft\Fias\Component\Exception\PipeException;
+use Liquetsoft\Fias\Component\FiasInformer\InformerResponse;
+use Liquetsoft\Fias\Component\Pipeline\Pipe\Pipe;
+use Liquetsoft\Fias\Component\Pipeline\State\ArrayState;
+use Liquetsoft\Fias\Component\Pipeline\Task\Task;
+use Liquetsoft\Fias\Component\FiasInformer\FiasInformer;
+use Liquetsoft\Fias\Component\VersionManager\VersionManager;
+use RuntimeException;
+
+/**
+ * Консольная команда, которая отображает текущую версию, полную версию
+ * и список версий на обновление.
+ */
+class VersionCommand extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature = 'liquetsoft:fias:versions';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Show information about current version, delta versions and full version.';
+
+    /**
+     * @var FiasInformer
+     */
+    private $informer;
+
+    /**
+     * @var VersionManager
+     */
+    private $versionManager;
+
+    public function __construct(FiasInformer $informer, VersionManager $versionManager)
+    {
+        parent::__construct();
+
+        $this->informer = $informer;
+        $this->versionManager = $versionManager;
+    }
+
+    /**
+     * Запуск команды на исполнение.
+     */
+    public function handle(): void
+    {
+
+    }
+}
