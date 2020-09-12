375328f54db0d0387fb50160cc34655273d62424
marvin255 <postbox433@gmail.com>
2020-09-13 00:17:52 +0200

    add versions command

--- src/Command/VersionsCommand.php
+++ src/Command/VersionsCommand.php
@@ -5,16 +5,9 @@ declare(strict_types=1);
 namespace Liquetsoft\Fias\Laravel\LiquetsoftFiasBundle\Command;
 
 use Illuminate\Console\Command;
-use Illuminate\Foundation\Application;
-use Liquetsoft\Fias\Component\Exception\PipeException;
-use Liquetsoft\Fias\Component\FiasInformer\InformerResponse;
-use Liquetsoft\Fias\Component\Pipeline\Pipe\Pipe;
-use Liquetsoft\Fias\Component\Pipeline\State\ArrayState;
-use Liquetsoft\Fias\Component\Pipeline\Task\Task;
 use Liquetsoft\Fias\Component\FiasInformer\FiasInformer;
-use Liquetsoft\Fias\Component\VersionManager\VersionManager;
 use Liquetsoft\Fias\Component\FiasInformer\InformerResponse;
-use RuntimeException;
+use Liquetsoft\Fias\Component\VersionManager\VersionManager;
 
 /**
  * Консольная команда, которая отображает текущую версию, полную версию
@@ -55,17 +48,58 @@ class VersionsCommand extends Command
      */
     public function handle(): void
     {
+        $currentVersion = $this->versionManager->getCurrentVersion();
+        if ($currentVersion->hasResult()) {
+            $this->showTable('Current version of FIAS', [$currentVersion]);
+        } else {
+            $this->showEmptyResponse('Current version of FIAS', 'FIAS is not installed');
+        }
 
+        $completeVersion = $this->informer->getCompleteInfo();
+        if ($completeVersion->hasResult()) {
+            $this->showTable('Complete version of FIAS', [$completeVersion]);
+        } else {
+            $this->showEmptyResponse('Complete version of FIAS', 'Complete version not found');
+        }
+
+        $deltaVersions = $this->informer->getDeltaList();
+        $this->showTable('Delta versions of FIAS', array_slice($deltaVersions, 0, 15));
+
+        $this->line('');
     }
 
     /**
      * Отображает список версий в виде таблицы.
      *
-     * @param string $header
+     * @param string             $header
      * @param InformerResponse[] $headers
      */
     private function showTable(string $header, array $versions): void
     {
+        $rows = [];
+        foreach ($versions as $version) {
+            $rows[] = [
+                'Version' => $version->getVersion(),
+                'Url' => $version->getUrl(),
+            ];
+        }
 
+        $this->line('');
+        $this->info($header);
+        $this->table(['Version', 'Url'], $rows);
+    }
+
+    /**
+     * Отображает пустую таблицу.
+     *
+     * @param string $header
+     * @param string $message
+     */
+    private function showEmptyResponse(string $header, string $message = 'No data provided'): void
+    {
+        $this->line('');
+        $this->info($header);
+        $this->line('');
+        $this->error($message);
     }
 }
