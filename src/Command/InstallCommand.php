7bf1d3a30e2c4b6f7c0ca9eb0343b563d4664a24
liquetsoft <liquetsoft@protonmail.com>
2024-09-11 16:22:15 +0200

    Fix install command

--- src/Command/InstallCommand.php
+++ src/Command/InstallCommand.php
@@ -13,22 +13,13 @@ use Liquetsoft\Fias\Component\Pipeline\State\ArrayState;
 /**
  * Консольная команда для установки ФИАС с ноля.
  */
-class InstallCommand extends Command
+final class InstallCommand extends Command
 {
-    /**
-     * @var string
-     */
     protected $signature = 'liquetsoft:fias:install';
 
-    /**
-     * @var string|null
-     */
     protected $description = 'Installs full version of FIAS from scratch.';
 
-    /**
-     * @var Pipe
-     */
-    protected $pipeline;
+    private readonly Pipe $pipeline;
 
     /**
      * В конструкторе передаем ссылку на пайплайн установки.
@@ -46,19 +37,20 @@ class InstallCommand extends Command
      */
     public function handle(): void
     {
-        $this->info('Installing full version of FIAS.');
+        $this->info('Installing full version of FIAS');
         $start = microtime(true);
 
-        $state = new ArrayState();
-
         try {
-            $this->pipeline->run($state);
+            $this->pipeline->run(new ArrayState());
         } catch (\Throwable $e) {
-            $message = "Something went wrong during the installation. Please check the Laravel's log to get more information.";
-            throw new FiasConsoleException($message, 0, $e);
+            throw new FiasConsoleException(
+                "Something went wrong during the installation. Please check the Laravel's log to get more information",
+                0,
+                $e
+            );
         }
 
         $total = round(microtime(true) - $start, 4);
-        $this->info("Full version of FIAS installed after {$total} s.");
+        $this->info("Full version of FIAS installed after {$total} s");
     }
 }
