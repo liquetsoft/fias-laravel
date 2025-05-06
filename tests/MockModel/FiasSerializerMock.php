ccf34705cdd3ff5ace22c769250a52a2a26ca8ce
liquetsoft <liquetsoft@protonmail.com>
2025-05-06 18:28:14 +0200

    Add properties to mock item

--- tests/MockModel/FiasSerializerMock.php
+++ tests/MockModel/FiasSerializerMock.php
@@ -8,6 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * Мок для проверки десериализации.
+ *
+ * @property int                $ACTSTATID
+ * @property string             $name
+ * @property float              $floatNum
+ * @property bool               $boolVal
+ * @property \DateTimeInterface $test_date_val
+ * @property int                $timestamp
+ * @property string|null        $nullableCast
+ * @property string|null        $defaultItem
  */
 final class FiasSerializerMock extends Model
 {
